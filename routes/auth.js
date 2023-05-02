90ec3060b150a8205b4ac108b1611018c3f517aa
PrinceStiles <princekarpture@gmail.com>
2023-05-02 12:22:01 +0000

    register and login requirements

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,42 @@
 const router = require("express").Router();
+const User = require("../modules/User");
+const bcrypt = require("bcrypt")
 
-router.get("/", (req, res) => {
-    res.send("welcome to auth page.")
+// REGISTER
+router.post("/register", async (req, res) => {
+    try{
+        // bcrypt user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        // create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        })
+
+        // save user and post req
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+    }
+    
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(404).send("user not found");
+
+        const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+        !isValidPassword && res.status(400).send("wrong password");
+
+        res.status(200).send(user);
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 module.exports = router;
