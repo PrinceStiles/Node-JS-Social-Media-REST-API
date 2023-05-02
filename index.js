d91d2744a367ad77ac09cf4cfd1fa426c27c07d7
PrinceStiles <princekarpture@gmail.com>
2023-05-02 09:25:52 +0000

    Middlewares and first request

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL);
 
+// middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("welcome to homepage")
+})
+
 app.listen(8800, () => {
     console.log("Backend Server Is Running...")
 })
